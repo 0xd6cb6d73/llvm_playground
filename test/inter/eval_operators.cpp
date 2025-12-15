2e6ebe9a2f94b47229193a54e4e44efa7b6a4a75
0xd6cb6d73 <72226198+0xd6cb6d73@users.noreply.github.com>
2025-12-15 21:27:58 +0100

    Add recursive operations

--- test/inter/eval_operators.cpp
+++ test/inter/eval_operators.cpp
@@ -14,6 +14,18 @@ TEST(inter, eval_op_plus) {
   EXPECT_EQ(std::get<int32_t>(val), 2);
 }
 
+TEST(inter, eval_op_plus_plus) {
+  lang::Interpreter inter;
+
+  auto lhs = lang::ParseExpVal(1);
+  auto pack_1 = lang::ParseExpVal(lang::ParseExp{
+      .type = lang::ParseExpType::OperatorPlus, .lhs = &lhs, .rhs = &lhs});
+  const auto pack = lang::ParseExp{
+      .type = lang::ParseExpType::OperatorPlus, .lhs = &lhs, .rhs = &pack_1};
+  const auto val = inter.eval(pack);
+  EXPECT_EQ(std::get<int32_t>(val), 3);
+}
+
 TEST(inter, eval_op_minus) {
   lang::Interpreter inter;
   auto lhs = lang::ParseExpVal(8);
@@ -22,6 +34,18 @@ TEST(inter, eval_op_minus) {
       .type = lang::ParseExpType::OperatorMinus, .lhs = &lhs, .rhs = &rhs};
   const auto val = inter.eval(pack);
   EXPECT_EQ(std::get<int32_t>(val), 2);
+}
+
+TEST(inter, eval_op_minus_minus) {
+  lang::Interpreter inter;
+  auto lhs = lang::ParseExpVal(8);
+  auto rhs = lang::ParseExpVal(6);
+  auto pack_1 = lang::ParseExpVal(lang::ParseExp{
+      .type = lang::ParseExpType::OperatorMinus, .lhs = &lhs, .rhs = &rhs});
+  const auto pack = lang::ParseExp{
+      .type = lang::ParseExpType::OperatorMinus, .lhs = &lhs, .rhs = &pack_1};
+  const auto val = inter.eval(pack);
+  EXPECT_EQ(std::get<int32_t>(val), 6);
 }
 
 TEST(inter, eval_op_mul) {
@@ -34,6 +58,18 @@ TEST(inter, eval_op_mul) {
   EXPECT_EQ(std::get<int32_t>(val), 2);
 }
 
+TEST(inter, eval_op_mul_mul) {
+  lang::Interpreter inter;
+  auto lhs = lang::ParseExpVal(2);
+  auto rhs = lang::ParseExpVal(1);
+  auto pack_1 = lang::ParseExpVal(lang::ParseExp{
+      .type = lang::ParseExpType::OperatorMul, .lhs = &lhs, .rhs = &rhs});
+  const auto pack = lang::ParseExp{
+      .type = lang::ParseExpType::OperatorMul, .lhs = &lhs, .rhs = &pack_1};
+  const auto val = inter.eval(pack);
+  EXPECT_EQ(std::get<int32_t>(val), 4);
+}
+
 TEST(inter, eval_op_div) {
   lang::Interpreter inter;
   auto lhs = lang::ParseExpVal(6);
@@ -42,4 +78,16 @@ TEST(inter, eval_op_div) {
       .type = lang::ParseExpType::OperatorDiv, .lhs = &lhs, .rhs = &rhs};
   const auto val = inter.eval(pack);
   EXPECT_EQ(std::get<int32_t>(val), 2);
+}
+
+TEST(inter, eval_op_div_div) {
+  lang::Interpreter inter;
+  auto lhs = lang::ParseExpVal(6);
+  auto rhs = lang::ParseExpVal(3);
+  auto pack_1 = lang::ParseExpVal(lang::ParseExp{
+      .type = lang::ParseExpType::OperatorDiv, .lhs = &lhs, .rhs = &rhs});
+  const auto pack = lang::ParseExp{
+      .type = lang::ParseExpType::OperatorDiv, .lhs = &lhs, .rhs = &pack_1};
+  const auto val = inter.eval(pack);
+  EXPECT_EQ(std::get<int32_t>(val), 3);
 }
