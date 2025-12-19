1350c2993181683f607c766456a8b465132523d4
0xd6cb6d73 <72226198+0xd6cb6d73@users.noreply.github.com>
2025-12-19 21:28:04 +0100

    Add mul/div expression parsing test

--- test/parser/parse_operators.cpp
+++ test/parser/parse_operators.cpp
@@ -133,3 +133,49 @@ TEST(parser, parse_operator_recurse_plus_minus) {
   EXPECT_EQ(outer_lhs_rhs_lhs_rhs_lhs, 3);
   EXPECT_EQ(outer_lhs_rhs_lhs_rhs_rhs, 4);
 }
+
+TEST(parser, parse_operator_recurse_mul_div) {
+  lang::Parser parser;
+  //(+ (+ {lhs_lhs} (+ (+ {lhs_rhs_lhs} (+ {lhs_rhs_lhs_lhs} {lhs_rhs_lhs_rhs}))
+  //{lhs_rhs_rhs})) {rhs})
+  auto ret = parser.parse(
+      std::format("(* (/ {} (* (/ {} (* {} {})) {})) {})", 1, 2, 3, 4, 5, 6));
+
+  const auto outer_type = ret.type;
+  std::unique_ptr<lang::ParseExp> outer_lhs =
+      std::move(std::get<std::unique_ptr<lang::ParseExp>>(ret.lhs));
+  const auto outer_rhs = std::get<intptr_t>(ret.rhs);
+
+  const auto outer_lhs_type = outer_lhs->type;
+  const auto outer_lhs_lhs = std::get<intptr_t>(outer_lhs->lhs);
+  const std::unique_ptr<lang::ParseExp> outer_lhs_rhs =
+      std::move(std::get<std::unique_ptr<lang::ParseExp>>(outer_lhs->rhs));
+
+  const auto outer_lhs_rhs_type = outer_lhs_rhs->type;
+  const std::unique_ptr<lang::ParseExp> outer_lhs_rhs_lhs =
+      std::move(std::get<std::unique_ptr<lang::ParseExp>>(outer_lhs_rhs->lhs));
+  const auto outer_lhs_rhs_rhs = std::get<intptr_t>(outer_lhs_rhs->rhs);
+
+  const auto outer_lhs_rhs_lhs_type = outer_lhs_rhs_lhs->type;
+  const auto outer_lhs_rhs_lhs_lhs = std::get<intptr_t>(outer_lhs_rhs_lhs->lhs);
+  const std::unique_ptr<lang::ParseExp> outer_lhs_rhs_lhs_rhs = std::move(
+      std::get<std::unique_ptr<lang::ParseExp>>(outer_lhs_rhs_lhs->rhs));
+
+  const auto outer_lhs_rhs_lhs_rhs_type = outer_lhs_rhs_lhs_rhs->type;
+  const auto outer_lhs_rhs_lhs_rhs_lhs =
+      std::get<intptr_t>(outer_lhs_rhs_lhs_rhs->lhs);
+  const auto outer_lhs_rhs_lhs_rhs_rhs =
+      std::get<intptr_t>(outer_lhs_rhs_lhs_rhs->rhs);
+
+  EXPECT_EQ(outer_type, lang::ParseExpType::OperatorMul);
+  EXPECT_EQ(outer_lhs_type, lang::ParseExpType::OperatorDiv);
+  EXPECT_EQ(outer_lhs_rhs_type, lang::ParseExpType::OperatorMul);
+  EXPECT_EQ(outer_lhs_rhs_lhs_type, lang::ParseExpType::OperatorDiv);
+  EXPECT_EQ(outer_lhs_rhs_lhs_rhs_type, lang::ParseExpType::OperatorMul);
+  EXPECT_EQ(outer_rhs, 6);
+  EXPECT_EQ(outer_lhs_lhs, 1);
+  EXPECT_EQ(outer_lhs_rhs_rhs, 5);
+  EXPECT_EQ(outer_lhs_rhs_lhs_lhs, 2);
+  EXPECT_EQ(outer_lhs_rhs_lhs_rhs_lhs, 3);
+  EXPECT_EQ(outer_lhs_rhs_lhs_rhs_rhs, 4);
+}
