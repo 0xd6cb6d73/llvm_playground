fa33f57dd30b69435f88a9bdeb5391772d62f159
0xd6cb6d73 <72226198+0xd6cb6d73@users.noreply.github.com>
2026-02-24 22:49:36 +0100

    Remove old stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "compiler.hpp"
 #include "inter.hpp"
 
 #include "parser.hpp"
@@ -6,7 +7,9 @@
 int main() {
   lang::Parser parser;
   lang::Interpreter inter;
-  const auto exp = parser.parse(std::string(R"((+ 1 1))"));
-  const auto ret = inter.eval(exp);
+  lang::comp::Compiler compiler;
+  auto exp = std::make_unique<lang::ParseExp>(parser.parse(std::string(R"((+ 1 1))")));
+  const auto ret = inter.eval(*exp);
+  const auto comp = compiler.LowerToLLVM(std::move(exp), "main_eval.ll");
   return std::get<intptr_t>(ret);
 }
